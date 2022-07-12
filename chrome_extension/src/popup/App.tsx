f29ae4bb3adf88cc51306f1248a086eaac4df876
osadavc <osadavidath@gmail.com>
2022-07-13 03:11:52 +0530

    🔧 Configured Chrome Extension

--- chrome_extension/src/popup/App.tsx
+++ chrome_extension/src/popup/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
@@ -27,7 +24,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +36,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- chrome_extension/vite.config.ts
+++ chrome_extension/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { chromeExtension } from "vite-plugin-chrome-extension";
 
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: "src/manifest.json",
+    },
+  },
+  plugins: [react(), chromeExtension()],
 });
