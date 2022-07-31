29cb366730eb6dfa97e0d1ca52a03d2d10cbdcc9
osadavc <osadavidath@gmail.com>
2022-07-31 11:15:33 +0530

    🔧 Update Extension Manifest

--- chrome_extension/src/popup/services/apiService.ts
+++ chrome_extension/src/popup/services/apiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "https://twotion.osadavidath.com/api";
 
 export const client = axios.create({
   baseURL: BASE_URL,
